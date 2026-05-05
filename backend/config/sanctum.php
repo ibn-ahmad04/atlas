ff4c5ac927746dde85e4f5ff00995ca989b55273
Mouhammad Youssouf Dioum <mouhammad.dioum04@gmail.com>
2026-05-05 09:58:01 +0000

    feat: auth Sanctum register login logout me - token 7j expiration

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -47,7 +47,7 @@ return [
     |
     */
 
-    'expiration' => null,
+    'expiration' => 60 * 24 * 7,  // Changé de null à 60 * 24 * 7 (7 jours)
 
     /*
     |--------------------------------------------------------------------------
@@ -62,7 +62,7 @@ return [
     |
     */
 
-    'token_prefix' => env('SANCTUM_TOKEN_PREFIX', ''),
+    'token_prefix' => env('SANCTUM_TOKEN_PREFIX', 'atlas_'),
 
     /*
     |--------------------------------------------------------------------------
